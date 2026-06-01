573174e877552d83a9af992bc79c538c37afb7a8
Aman Rajpoot <vkt583059@email.com>
2026-06-01 11:28:16 +0530

    change title

--- main.py
+++ main.py
@@ -187,22 +187,22 @@ def main():
                 st.metric("Balance Status", st.session_state.balance_status)
 
     st.markdown("""
-    <div style="
-    text-align:center;
-    padding:25px;
-    border-radius:20px;
-    background:linear-gradient(135deg,#0f172a,#1e293b);
-    margin-bottom:20px;
-    ">
-    <h1 style="color:white;">
-    🏋️ AI Gym Coach
-    </h1>
+            <div style="
+            text-align:center;
+            padding:25px;
+            border-radius:20px;
+            background:linear-gradient(135deg,#0f172a,#1e293b);
+            margin-bottom:20px;
+            ">
+            <h1 style="color:white;">
+            🏋️ AI Gym Coach
+            </h1>
 
-    <p style="color:#cbd5e1;font-size:18px;">
-    Real-Time Pose Detection • AI Voice Coaching • Rep Tracking
-    </p>
-    </div>
-    """, unsafe_allow_html=True)
+            <p style="color:#cbd5e1;font-size:18px;">
+            Real-Time Pose Detection • AI Voice Coaching • Rep Tracking
+            </p>
+            </div>
+            """, unsafe_allow_html=True)
  
     if st.session_state.get("audio_to_play"):
         autoplay_audio(st.session_state.audio_to_play)
--- services/auth/login_wall.py
+++ services/auth/login_wall.py
@@ -5,11 +5,24 @@ def render_login_wall():
     if st.session_state.get('user_id') is not None:
         return True
     
+    st.markdown("""
+<div style="
+text-align:center;
+padding:25px;
+border-radius:20px;
+background:linear-gradient(135deg,#0f172a,#1e293b);
+margin-bottom:20px;
+">
+<h1 style="color:white;">
+🏋️ AI Gym Coach
+</h1>
 
-
-    st.title("🏋AI Real-time GYM Trainer")
-    st.markdown("### welcome! please enter a username to start.")
-
+<p style="color:#cbd5e1;font-size:18px;">
+Real-Time Pose Detection • AI Voice Coaching • Rep Tracking
+</p>
+</div>
+""", unsafe_allow_html=True)
+    
 
     with st.form('login_form', clear_on_submit=False):
         username=st.text_input("name (unique)",placeholder="unique name e.g Sharma")
